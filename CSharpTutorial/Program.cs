d5672854cda99b6f910bdad3bd1ce3293a92627b
Reid South <rbsouth@gmail.com>
2020-02-25 22:23:05 -0700

    calculator

--- CSharpTutorial/Program.cs
+++ CSharpTutorial/Program.cs
@@ -6,11 +6,12 @@ namespace CSharpTutorial
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter Your Name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
-
+            Console.WriteLine(num1 + num2);
 
             Console.ReadLine();
         }
